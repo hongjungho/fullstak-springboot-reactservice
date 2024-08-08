090be17ef8266a7bf1684d962d78f424b650c2e9
hongjungho <cvb1221@naver.com>
2024-08-08 15:03:17 +0900

    git branch test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> goodjob2332232323232.
+          Edit <code>src/App.js</code> git branch test
         </p>
         <a
           className="App-link"
