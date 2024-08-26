de28648895e849d1a20f4256a33615404f0614ec
yawooch <yoc0421@naver.com>
2024-08-26 16:58:56 +0900

    긴급 복구 ㅋ

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,14 @@
 import logo from '../logo2.svg';
-import { Link, useNavigation } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = ()=>{
 
-    const navi = useNavigation();
+    const navi = useNavigate();
+
+    function onClickLogin()
+    {
+        navi('/')
+    }
 
     return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -34,7 +39,7 @@ const Login = ()=>{
             </div>
 
             <div>
-                <button type="button" className="flex w-full justify-center rounded-md bg-lime-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600" onClick={()=>{navi('/')}}>Sign in</button>
+                <button type="button" className="flex w-full justify-center rounded-md bg-lime-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600" onClick={onClickLogin}>Sign in</button>
             </div>
             </form>
 
