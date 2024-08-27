cd10b91163291925e8afbebaaccec47e735c4b1a
yawooch <yoc0421@naver.com>
2024-08-27 17:32:32 +0900

    package.json 재업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {RouterProvider} from 'react-router-dom'
 import root from './router/root'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
