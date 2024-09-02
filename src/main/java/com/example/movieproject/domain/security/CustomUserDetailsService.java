7d0414e8d98499bed293a96165f78e85e467a27e
hongjungho <cvb1221@naver.com>
2024-09-02 15:34:11 +0900

    spring security login

--- src/main/java/com/example/movieproject/domain/security/CustomUserDetailsService.java
+++ src/main/java/com/example/movieproject/domain/security/CustomUserDetailsService.java
@@ -3,12 +3,14 @@ package com.example.movieproject.domain.security;
 import com.example.movieproject.domain.member.Member;
 import com.example.movieproject.domain.member.MemberDTO;
 import com.example.movieproject.domain.member.MemberRepository;
-import io.micrometer.observation.annotation.Observed;
+import com.example.movieproject.domain.member.MemberRole;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
@@ -20,11 +22,35 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("--------------loadUserByUsername Start----------------");
+
+        log.info("----------------------test data insert-------------------------");
+        if(memberRepository.findAll().size() < 1) {
+            for (int i = 0; i < 10 ; i++) {
+                Member member = Member.builder()
+                        .email ("user"+i+"@test.com")
+                        .pw(passwordEncoder.encode("1111"))
+                        .nickname ("USER"+i)
+                        .build();
+                member.addRole(MemberRole.USER);
+                if(i >= 5) {
+                    member.addRole(MemberRole.MANAGER);
+                    if (i >= 8) {
+                        member.addRole(MemberRole.ADMIN);
+                    }
+                }
+                memberRepository.save(member);
+            }
+        }
+
+
 
         Member member = memberRepository.getWithRoles(username);
         if(member == null){
--- src/test/java/com/example/movieproject/MovieProjectApplicationTests.java
+++ src/test/java/com/example/movieproject/MovieProjectApplicationTests.java
@@ -3,12 +3,11 @@ package com.example.movieproject;
 import com.example.movieproject.domain.member.Member;
 import com.example.movieproject.domain.member.MemberRepository;
 import com.example.movieproject.domain.member.MemberRole;
-import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
 @Log4j2
