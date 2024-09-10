2500e2b872c48a488803bf5d8839941ccc0f8719
hongjungho <cvb1221@naver.com>
2024-09-10 14:20:06 +0900

    feat : JWT 로그인 구현
    back-end 구축 완료

--- src/main/java/com/example/movieproject/domain/member/MemberController.java
+++ src/main/java/com/example/movieproject/domain/member/MemberController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -17,7 +18,7 @@ import java.util.Map;
 public class MemberController {
 
     @RequestMapping("/api/member/refresh")
-    public Map<String, Object> refresh(@RequestHeader("Authorization")String authHeader, String refreshToken){
+    public Map<String, Object> refresh(@RequestHeader("Authorization")String authHeader, @RequestParam("refreshToken")String refreshToken){
         if(refreshToken == null) {
             throw new CustomJWTException("NULL_REFRASH");
         }
@@ -55,8 +56,8 @@ public class MemberController {
     private boolean checkExpiredToken(String token) {
         try {
             JWTUtil.validateToken(token);
-        } catch (CustomJWTException ex) {
-            if (ex.getMessage().equals("Expired")) {
+        }catch(CustomJWTException ex){
+            if(ex.getMessage().equals("Expired")){
                 return true;
             }
         }
