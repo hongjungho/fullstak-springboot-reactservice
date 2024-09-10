4f79ae082d7ee71c5d6e49c9c4fd8cf45bd33abf
yawooch <yoc0421@naver.com>
2024-09-10 21:22:42 +0900

    Revert "240902까지 작업 커밋"
    This reverts commit f3a19ab2a8ffc30f87e33bae04ddc11cee9b2185.

--- frontend/src/router/root.js
+++ frontend/src/router/root.js
@@ -1,15 +1,20 @@
 import { Suspense, lazy } from "react";
-import ProgresSpin from "../components/ProgresSpin";
 const {createBrowserRouter} = require("react-router-dom");
 
-const Loading = <ProgresSpin/>
+const Loading =
+<div className="flex items-center justify-center">
+    <h3 className="text-3xl font-bold items-center flex ">로딩중 입니다...</h3><br/>
+        <svg className="animate-spin ml-1 mr-1 h-32 w-32 flex items-center  text-teal-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+</div>
 
 const Main       = lazy(()=>import("../pages/MainPage"))
 const Mypage     = lazy(()=>import("../pages/MyPage"))
 const Board      = lazy(()=>import("../pages/board/BoardIndex"))
 const BrdDetail  = lazy(()=>import("../pages/board/BoardDetail"))
 const BrdEdit    = lazy(()=>import("../pages/board/BoardEdit"))
-const Musics     = lazy(()=>import("../pages/musics/MusicIndex"))
 const Login      = lazy(()=>import("../pages/Login"))
 
 const root = createBrowserRouter([
@@ -24,10 +29,6 @@ const root = createBrowserRouter([
     {
         path:"login",
         element:<Suspense fallback={Loading}><Login/></Suspense>
-    },
-    {
-        path:"musics",
-        element:<Suspense fallback={Loading}><Musics/></Suspense>
     },
     {
         path:"board",
